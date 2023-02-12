683ce407969471194e3ee669697e9e1e2097096f
Abi Chang <abi@cloud-latitude.com>
2023-02-12 23:54:45 +0800

    remove constructor

--- src/main/java/com/example/budge/homework/Budget.java
+++ src/main/java/com/example/budge/homework/Budget.java
@@ -3,14 +3,12 @@ package com.example.budge.homework;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 
 @Builder(toBuilder = true)
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Budget {
 
