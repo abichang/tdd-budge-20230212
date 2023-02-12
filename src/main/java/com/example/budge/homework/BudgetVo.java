40f6b380e43b201c8ffe8e9d4911ce9906b0e4d9
Abi Chang <abi@cloud-latitude.com>
2023-02-12 23:32:41 +0800

    extract field

--- src/main/java/com/example/budge/homework/BudgetVo.java
+++ src/main/java/com/example/budge/homework/BudgetVo.java
@@ -10,14 +10,16 @@ public class BudgetVo {
     private YearMonth yearMonth;
 
     private Integer amount;
+    private Period period;
 
     public BudgetVo(YearMonth yearMonth, Integer amount) {
         this.yearMonth = yearMonth;
         this.amount = amount;
+        this.period = new Period(this.yearMonth.atDay(1), this.yearMonth.atEndOfMonth());
     }
 
     public Period getPeriod() {
-        return new Period(this.yearMonth.atDay(1), this.yearMonth.atEndOfMonth());
+        return period;
     }
 
     public int getDailyAmount() {
