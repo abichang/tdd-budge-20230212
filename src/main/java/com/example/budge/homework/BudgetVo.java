9fa0313bd39dd0de210fcd032037de6c843ad73e
Abi Chang <abi@cloud-latitude.com>
2023-02-12 23:30:43 +0800

    rearrange constructors

--- src/main/java/com/example/budge/homework/BudgetVo.java
+++ src/main/java/com/example/budge/homework/BudgetVo.java
@@ -1,21 +1,22 @@
 package com.example.budge.homework;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.YearMonth;
 
 @Builder(toBuilder = true)
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class BudgetVo {
 
     private YearMonth yearMonth;
 
     private Integer amount;
+
+    public BudgetVo(YearMonth yearMonth, Integer amount) {
+        this.yearMonth = yearMonth;
+        this.amount = amount;
+    }
 
     public Period getPeriod() {
         return new Period(this.yearMonth.atDay(1), this.yearMonth.atEndOfMonth());
