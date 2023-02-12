0ab80260cd4a4de8d5280b86be26f37325e66ff5
Abi Chang <abi@cloud-latitude.com>
2023-02-12 23:55:12 +0800

    transform

--- src/main/java/com/example/budge/homework/Budget.java
+++ src/main/java/com/example/budge/homework/Budget.java
@@ -1,6 +1,5 @@
 package com.example.budge.homework;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,7 +8,6 @@ import java.time.format.DateTimeFormatter;
 
 @Builder(toBuilder = true)
 @Data
-@AllArgsConstructor
 public class Budget {
 
     private static final DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMM");
@@ -17,6 +15,11 @@ public class Budget {
     private String yearMonth;
 
     private Integer amount;
+
+    public Budget(String yearMonth, Integer amount) {
+        this.yearMonth = yearMonth;
+        this.amount = amount;
+    }
 
     public long getOverlappingAmount(Period period) {
         Period budgetPeriod = new Period(parseYearMonth().atDay(1), parseYearMonth().atEndOfMonth());
