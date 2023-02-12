996b5f163997db61113143e02b54f3cc16e81d42
Abi Chang <abi@cloud-latitude.com>
2023-02-12 13:26:39 +0800

    make it final

--- src/main/java/com/example/budge/homework/BudgetCalculator.java
+++ src/main/java/com/example/budge/homework/BudgetCalculator.java
@@ -15,10 +15,9 @@ import static java.util.stream.Collectors.toList;
 @Slf4j
 public class BudgetCalculator {
 
+    private final BudgetRepo budgetRepo;
     private DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMM");
     private DateTimeFormatter df2 = DateTimeFormatter.ofPattern("yyyyMMdd");
-
-    private BudgetRepo budgetRepo;
 
     public BudgetCalculator(BudgetRepo budgetRepo) {
         this.budgetRepo = budgetRepo;
